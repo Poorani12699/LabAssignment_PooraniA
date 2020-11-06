e04b1e8c78c93942e1c620f1a3f2efc3641553c7
Poorani A <48946039+Poorani12699@users.noreply.github.com>
2020-11-06 12:13:06 +0530

    Update ArraySort.java

--- LabAssignment_4-11-2020/src/ArraySort.java
+++ LabAssignment_4-11-2020/src/ArraySort.java
@@ -10,7 +10,7 @@ public class ArraySort
 	public static void main(String[] args) 
 	{
 		{
-			Integer[] intArray = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+			Integer[] intArray = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };// input array
 			System.out.println("Original Array:");
 			for (int i = 0; i < intArray.length; i++)
 				System.out.print(intArray[i] + "  ");
@@ -18,7 +18,7 @@ public class ArraySort
 			String strArray[] = new String[intArray.length];
 			for (int i = 0; i < intArray.length; i++)
 				strArray[i] = String.valueOf(intArray[i]);
-			System.out.println(Arrays.toString(strArray));
+			System.out.println(Arrays.toString(strArray));//printing sorted string
 		}
 	}
 
